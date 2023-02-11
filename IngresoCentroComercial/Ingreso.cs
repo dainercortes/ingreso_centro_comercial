d36384690ba2f548320f7b6b742a890933cf8978
Dainer Cortes <dainer.ramirez2003@gmail.com>
2023-02-11 07:33:34 -0500

    Cargando codigo

---
+++ IngresoCentroComercial/Ingreso.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IngresoCentroComercial
+{
+    internal class Ingreso
+    {
+        //Atributo de la clase
+        private int dia;
+        private int digito;
+
+        //Metodo contructor
+        public Ingreso()
+        {
+            dia = 0;
+            digito = 0;
+        }
+
+        //Metodos de la clase
+        public void setDia(int d)
+        {
+            d = dia;
+        }
+        public void setDigito(int g)
+        {
+            g = digito;
+        }
+        public int getDia()
+        {
+            return dia;
+        }
+        public int getDigito()
+        {
+            return digito;
+        }
+        //Metodo para ingresar los datos solicitados
+        public void IngresarDatos()
+        {
+            Console.WriteLine("\n Ingrese los datos para saber si puede ingresar al centro comercial");
+
+            Console.Write("\n Día actual: ");
+            dia = int.Parse(Console.ReadLine());
+
+            Console.Write("\n Ultimo dígito de la cédula: ");
+            digito = int.Parse(Console.ReadLine());
+        }
+        public void VerificarIngreso()
+        {
+            //Calcula si el dia y el digito son pares
+            if (dia % 2 == 0 && digito % 2 == 0)
+            {
+                Console.WriteLine("\n Puede Ingresar al centro comercial");
+            }
+            //Calcula si el dia y el digito son impares
+            else if (dia % 2 != 0 && digito % 2 != 0)
+            {
+                Console.WriteLine("\n Puede ingresar al centro comercial");
+            }
+            //Si las condiciones no se cumplen muestra que no puede ingresar 
+            else
+            {
+                Console.WriteLine("\n No puede ingresar al centro comercial");
+            }
+        }
+    }
+}
---
+++ IngresoCentroComercial/Program.cs
@@ -0,0 +1,11 @@
+﻿using IngresoCentroComercial;
+
+//Instancia de objetos
+Ingreso ic = new Ingreso();
+
+//Invocacion de metodos de la clase
+ic.IngresarDatos();
+ic.VerificarIngreso();
+
+//Presiona para cerrar el programa
+Console.ReadKey();
